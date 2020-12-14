cfe9833c0929a556e3ade8d67141523303cdb9fe
Luca <luca.tricerri@iit.it>
2020-12-14 15:42:21 +0100

    fix

--- src/unittest/test1.cpp
+++ src/unittest/test1.cpp
@@ -6,8 +6,46 @@
 
 TEST(Calc, Calc_001) {
     Calculate calc;
-    embot::dsp::Q15 calibQ15[]={0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1};    
-    embot::dsp::Q15 tareQ15[]={0x1,0x1,0x1,0x1,0x1,0x1};
+    embot::dsp::Q15 calibQ15[]={
+                    (short int)0x02b9,
+                    (short int)0xfff8,
+                    (short int)0x1bad,
+                    (short int)0x000d,
+                    (short int)0x01d2,
+                    (short int)0xe477,
+                    (short int)0x0676,
+                    (short int)0x1e87,
+                    (short int)0xf0cb,
+                    (short int)0x0095,
+                    (short int)0x058c,
+                    (short int)0xf0ac,
+                    (short int)0x50f8,
+                    (short int)0x00ad,
+                    (short int)0x01ac,
+                    (short int)0x3f90,
+                    (short int)0x3f45,
+                    (short int)0x015f,
+                    (short int)0x0012,
+                    (short int)0x0034,
+                    (short int)0xffe6,
+                    (short int)0xff12,
+                    (short int)0x00f9,
+                    (short int)0xffe9,
+                    (short int)0xfebc,
+                    (short int)0x0007,
+                    (short int)0xffca,
+                    (short int)0x009c,
+                    (short int)0x00ae,
+                    (short int)0x0032,
+                    (short int)0xfffe,
+                    (short int)0x003f,
+                    (short int)0x004d,
+                    (short int)0xfff9,
+                    (short int)0x0003,
+                    (short int)0x0052
+                    };    
+
+    embot::dsp::Q15 tareQ15[]={0,0,0,0,0,0};//{0.005126,0.01123,-0.0087,0.0170,0,00073,-0,0053};
     calc.handleCalibMatrixQ15_.load(6,6,calibQ15);
     calc.handleCalibTareQ15_.load(1,6,tareQ15);
 
