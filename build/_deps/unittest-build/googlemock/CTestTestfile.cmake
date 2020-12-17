90ee636c89ae952c6479f3482689637becc68b6b
Luca <luca.tricerri@iit.it>
2020-12-17 12:02:30 +0100

    fix

--- build/_deps/unittest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/triccyx/Documents/fake-strain2/build/_deps/unittest-src/googlemock
-# Build directory: /home/triccyx/Documents/fake-strain2/build/_deps/unittest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/_deps/unittest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/triccyx/Documents/fake-strain2/build/_deps/unittest-src/googletest
-# Build directory: /home/triccyx/Documents/fake-strain2/build/_deps/unittest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
